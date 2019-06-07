e236dcd478f90d469dfbd0f35bdcca9ed0e2317d
Misty DeRamus <mistyderamus@gmail.com>
2019-06-07 15:26:45 -0500

    daily journal 3 complete

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,45 +1,48 @@
 /*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned 
+    define your journal entry objects inside the journal entries array.
 */
-const objectsJournalEntry = {
-  dateOfEntry: "June 5, 2019",
-  titleOfEntry: "JavaScript Objects",
-  journalNote: "Objects have keys and values",
-  mood: "Happy"
+const journalEntries = [
+  {
+    dateOfEntry: "June 5, 2019",
+    titleOfEntry: "JavaScript Objects",
+    journalNote: "Objects have keys and values",
+    mood: "Happy"
+  },
+
+  {
+    dateOfEntry: "June 5, 2019",
+    titleOfEntry: "Arrays",
+    journalNote: "Arrays hold a list of common items",
+    mood: "Overwhelmed"
+  },
+
+  {
+    dateOfEntry: "June 5, 2019",
+    titleOfEntry: "Manipulating the DOM",
+    journalNote:
+      "Use querySelector or querySelectorAll to retrieve a single or list of components",
+    mood: "frustrated"
+  }
+];
+
+// write a function that builds a journal entry HTML string template. To create, and return, a string template that
+// represents a single journal entry object as HTML 
+
+const makeJournalEntryComponent = journalEntryObject => {
+  return `
+                    <div class="journalEntry">
+                        <h1>${journalEntryObject.titleOfEntry}</h1>
+                          <section>${journalEntryObject.journalNote}</section>
+                          <aside>${journalEntryObject.dateOfEntry}</aside>
+                      </div>
+                  `;
 };
 
-// const arraysJournalEntry = {
-//   dateOfEntry: "June 5, 2019",
-//   titleOfEntry: "Arrays",
-//   journalNote: "Arrays hold a list of common items",
-//   mood: "Overwhelmed"
-// };
+// stores a reference to the HTML element
+const renderJournalEntries = document.querySelector(".entryLog");
 
-// const domJournalEntry = {
-//   dateOfEntry: "June 5, 2019",
-//   titleOfEntry: "Manipulating the DOM",
-//   journalNote:
-//     "Use querySelector or querySelectorAll to retrieve a single or list of components",
-//   mood: "frustrated"
-// };
-
-const journalEntries = [];
-journalEntries.push({
-  dateOfEntry: "June 5, 2019",
-  titleOfEntry: "Manipulating the DOM",
-  journalNote:
-    "Use querySelector or querySelectorAll to retrieve a single or list of components",
-  mood: "frustrated"
-});
-journalEntries.push({
-  dateOfEntry: "June 6, 2019",
-  titleOfEntry: "New",
-  journalNote: "this is it",
-  mood: "excited"
-});
-
-// console.log(objectsJournalEntry);
-// console.log(arraysJournalEntry);
-// console.log(domJournalEntry);
-console.log(journalEntries);
+// iterate the array of journal entries and apply the style to the  HTML by calling the  makeJournalEntryComponent() function.
+for (let i = 0; i < journalEntries.length; i++) {
+  let journalEntry = journalEntries[i];
+  renderJournalEntries.innerHTML += makeJournalEntryComponent(journalEntry);
+}
