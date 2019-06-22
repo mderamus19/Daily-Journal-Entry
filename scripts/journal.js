b76545a44560c463d93d3b92917ff586d358ae52
Misty DeRamus <mistyderamus@gmail.com>
2019-06-22 16:57:59 -0500

    add button

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,15 +1,24 @@
-/*
-    Main application logic that uses the functions and objects
-    defined in the other JavaScript files.
 
-    Change the fake variable names below to what they should be
-    to get the data and display it.
-*/
-// API.methodToGetData().then(renderJournalEntries)
-    
+
+// In your main JavaScript module (journal.js) add a click event listener to the Record Journal Entry button at the bottom of your form. 
+// When the user clicks the button, you need to create a new entry in your API. The HTTP method that you use to create resources is POST. 
+// Guidance on syntax is provided below.
 
 API.getJournalEntries()
     .then(parsedentries => {
       renderJournalEntries(parsedentries)      // parsedentries holds the data received from api
     })
 
+document.querySelector("#save-btn").addEventListener("click", () => {
+
+    
+})
+// Invoke the factory function, passing along the form field values
+// const newJournalEntry = ??
+// This is a factory function, because it returns an object
+// function buildLegoObj(creatorName, color) {
+//     return ({
+//       creator: creatorName,
+//       color: color
+//     })
+//   }
